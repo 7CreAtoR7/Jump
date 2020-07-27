700a9ce1f4dbd876b3342440b5c26918abbbdde6
Egor <aptenn@pop-os.localdomain>
2020-07-27 18:34:27 +0300

    rotate

--- src/Actors/Player.gd
+++ src/Actors/Player.gd
@@ -1,18 +1,24 @@
 extends Actor
 
+const spd = 5
 
 func _physics_process(delta):
 	var is_jump_interrupted: = Input.is_action_just_released("jump") and _velocity.y < 0.0
 	var direction: = get_direction()
 	_velocity = calculate_move_velocity(_velocity, direction, speed, is_jump_interrupted)
 	_velocity = move_and_slide(_velocity, FLOOR_NORMAL)
-	
+	if direction.x != 0:
+		$player.rotation += direction.x * spd * delta
+	#	var rotation_to_mouse = $player.rotation - ROTATE
+	#$player.rotation = lerp_angle($player.rotation, rotation_to_mouse, 0.75)
 
 func get_direction() -> Vector2:
+	
 	return Vector2(
 		Input.get_action_strength("move_right") - Input.get_action_strength("move_left"),
 		-1.0 if Input.is_action_pressed("jump") and is_on_floor() else 1.0
-	)
+	).rotated(rotation)
+
 
 
 func calculate_move_velocity(
